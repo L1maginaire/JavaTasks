b860c1f05d0644c9a6071ae031fb1e798287e32d
Roman <rikser999@yandex.ru>
2017-10-13 19:19:50 +0200

    commit

---
+++ src/ThinkingInJava4thEd/b_Operators/t_09.java
@@ -0,0 +1,13 @@
+package ThinkingInJava4thEd.b_Operators;
+
+//p109
+// Display the largest and smallest numbers for both float and double exponential notation.
+
+public class t_09 {
+    public static void main(String[] args) {
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+    }
+}
