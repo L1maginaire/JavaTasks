c62d17c84fbf8ce238e5578bd9c05f280e12aab8
Roman <rikser999@yandex.ru>
2017-10-13 11:54:48 +0200

    Initial commit

---
+++ src/ThinkingInJava4thEd/a_Everything_is_an_object/_1.java
@@ -0,0 +1,9 @@
+package ThinkingInJava4thEd.a_Everything_is_an_object;
+
+public class _1 {
+    static int x;
+    static char y;
+    public static void main(String[] args) {
+        System.out.println("x: " + x + ", " + "y: "+ y);
+    }
+}
